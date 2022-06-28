6eb1af7a40e8427f6cda2fcfd8d9bee62d692416
sailingdev <sailingdev123@gmail.com>
2022-06-28 08:59:40 +0000

    edit accounts user urls

--- backend/accounts/urls.py
+++ backend/accounts/urls.py
@@ -1,8 +1,14 @@
 # backend.accounts.urls.py
 
-from django.urls import path
+from django.urls import path, include
 
+from accounts import routers, views
 
 urlpatterns = [
-    
+    path(
+        route='data/',
+        view=views.user_retrieve_update_destroy,
+        name='user-data'
+    ),
+    path('users/', include(routers.router.urls)),
 ]
