8912a11d51a6db14ada6acb77da15ccb4f3f6ed2
sailingdev <sailingdev123@gmail.com>
2022-06-28 08:43:01 +0000

    improve accounts models

--- backend/accounts/models.py
+++ backend/accounts/models.py
@@ -3,7 +3,7 @@
 from django.db import models
 from django.contrib.auth.models import AbstractUser
 
-from . import managers
+from accounts.managers import CustomUserManager
 
 NULL_AND_BLANK = {'null': True, 'blank': True}
 
@@ -37,7 +37,7 @@ class CustomUser(AbstractUser):
     USERNAME_FIELD = 'email'
     REQUIRED_FIELDS = []
 
-    objects = managers.CustomUserManager()
+    objects = CustomUserManager()
 
     class Meta:
         verbose_name_plural = 'accounts'
