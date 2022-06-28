bac4790b46619a7c9e906b2659108735470d03bb
sailingdev <sailingdev123@gmail.com>
2022-06-28 08:47:58 +0000

    edit accounts user retrieve update and destroy api view

--- backend/accounts/views.py
+++ backend/accounts/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+# backend.accounts.views.py
 
-# Create your views here.
+from django.contrib.auth import get_user_model
+from rest_framework import generics, permissions
+
+from accounts import serializers
+
+USER = get_user_model()
+
+
+class UserRetrieveUpdateDestroyAPIView(
+    generics.RetrieveUpdateDestroyAPIView
+):
+    queryset = USER.objects.all()
+    serialier_class = serializers.CustomUserSerializer
+    permission_classes = [permissions.IsAuthenticated]
+
+    def get_object(self):
+        return self.request.user
